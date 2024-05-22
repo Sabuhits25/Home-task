4b34dc58797d9a774dd9230b89ca20df47ffeb53
Sabuhits25 <safarlisabuhi17@gmail.com>
2024-05-22 15:48:47 +0400

    Add project files.

---
+++ Home task/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Home_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region arrayin tersine cevrilmesi
+            //int[] array = { 1, 2, 43, 567, 23, 21 };
+            //int i = 0;
+            //int third = 0;
+
+            //for (i = 0; i < array.Length / 2; i++)
+            //{
+            //    third = array[i];
+
+            //    array[i] = array[array.Length - i - 1];
+            //    array[array.Length - i - 1] = third;
+            //}
+            //for (i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+            #endregion 
+
+
+
+
+        }
+
+
+
+    }
+}
