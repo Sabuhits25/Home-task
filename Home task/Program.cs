1f7735b332acb68add9ee792ef4de9b50e57b236
Sabuhits25 <safarlisabuhi17@gmail.com>
2024-05-23 17:12:36 +0400

    complated

--- Home task/Program.cs
+++ Home task/Program.cs
@@ -5,22 +5,35 @@
         static void Main(string[] args)
         {
             #region arrayin tersine cevrilmesi
-            //int[] array = { 1, 2, 43, 567, 23, 21 };
+            //int[] numbers = { 1, 2, 43, 567, 23, 21 };
             //int i = 0;
             //int third = 0;
 
-            //for (i = 0; i < array.Length / 2; i++)
+            //for (i = 0; i < numbers.Length / 2; i++)
             //{
-            //    third = array[i];
+            //    third = numbers[i];
 
-            //    array[i] = array[array.Length - i - 1];
-            //    array[array.Length - i - 1] = third;
+            //    numbers[i] = numbers[numbers.Length - i - 1];
+            //    numbers[numbers.Length - i - 1] = third;
             //}
-            //for (i = 0; i < array.Length; i++)
+            //for (i = 0; i < numbers.Length; i++)
             //{
-            //    Console.WriteLine(array[i]);
+            //    Console.WriteLine(numbers[i]);
             //}
-            #endregion 
+            #endregion
+
+            #region oxsar reqemin bildirilmesi
+            //int[] numbers = { 1, 2, 43, 567, 23, 23 };
+            //int i = 0;
+            //for (i = 0; i < numbers.Length - 1; i++) 
+            //{
+            //    if (numbers[i] == numbers[i + 1])
+            //    {
+            //        Console.WriteLine($"Oxsar reqem var {numbers[i]}");
+            //    }
+            //}
+            #endregion
+
 
 
 
